5c335861dd1760cb9cda067868c23c4255c5f9ed
Greg MacWilliam <gmacwill77@gmail.com>
2020-01-09 10:10:34 -0500

    all parts of the dashboard.

--- lib/sidekiq/heroku_autoscale/web.rb
+++ lib/sidekiq/heroku_autoscale/web.rb
@@ -3,5 +3,5 @@ require 'sidekiq/heroku_autoscale/web_extension'
 
 if defined?(::Sidekiq::Web)
   ::Sidekiq::Web.register(::Sidekiq::HerokuAutoscale::WebExtension)
-  ::Sidekiq::Web.tabs["Scale"] = "autoscale"
+  ::Sidekiq::Web.tabs["Dynos"] = "dynos"
 end
--- lib/sidekiq/heroku_autoscale/web_extension.rb
+++ lib/sidekiq/heroku_autoscale/web_extension.rb
@@ -1,3 +1,5 @@
+require 'rack/file'
+
 module Sidekiq
   module HerokuAutoscale
     module WebExtension
@@ -5,9 +7,43 @@ module Sidekiq
       VIEW_PATH = File.join(File.expand_path('..', __FILE__), 'views')
 
       def self.registered(app)
-        app.get '/autoscale' do
-          # @cron_jobs = Sidekiq::HerokuAutoscale::Job.all
+        app.get '/dynos' do
+          @processes = {
+            default: 0,
+            low: 1
+          }
           render(:erb, File.read(File.join(VIEW_PATH, 'index.erb')))
+        end
+
+        app.get '/dynos/dashboard.js' do
+          headers = {
+            'Content-Type' => 'application/javascript',
+            'Cache-Control' => 'public, max-age=86400'
+          }
+
+          [200, headers, [File.read(File.join(VIEW_PATH, 'dashboard.js'))]]
+        end
+
+        app.get '/dynos/stats' do
+          sidekiq_stats = ::Sidekiq::Stats.new
+          json(
+            scale: {
+              default: 0,
+              low: 0,
+            },
+            sidekiq: {
+              processed: sidekiq_stats.processed,
+              failed: sidekiq_stats.failed,
+              busy: sidekiq_stats.workers_size,
+              processes: sidekiq_stats.processes_size,
+              enqueued: sidekiq_stats.enqueued,
+              scheduled: sidekiq_stats.scheduled_size,
+              retries: sidekiq_stats.retry_size,
+              dead: sidekiq_stats.dead_size,
+              default_latency: sidekiq_stats.default_queue_latency,
+            },
+            server_utc_time: server_utc_time
+          )
         end
       end
 
